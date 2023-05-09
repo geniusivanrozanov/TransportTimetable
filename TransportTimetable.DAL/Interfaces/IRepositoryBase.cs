55be84bcd5f8a54180d18c17f83325565f9d7423
Ivan Rozanov <navi.zor9@gmail.com>
2023-05-10 02:11:37 +0300

    add repository base with interface

---
+++ TransportTimetable.DAL/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace TransportTimetable.DAL.Interfaces;
+
+public interface IRepositoryBase<TEntity>
+{
+    IQueryable<TEntity> Get();
+    IQueryable<TEntity> Get(Expression<Func<TEntity, bool>> condition);
+    void Create(TEntity entity);
+    void Update(TEntity entity);
+    void Delete(TEntity entity);
+}
---
+++ TransportTimetable.DAL/Repositories/RepositoryBase.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+using TransportTimetable.DAL.Interfaces;
+
+namespace TransportTimetable.DAL.Repositories;
+
+public abstract class RepositoryBase<TEntity> : IRepositoryBase<TEntity> where TEntity : class
+{
+    private readonly RepositoryContext _context;
+
+    protected RepositoryBase(RepositoryContext context)
+    {
+        _context = context;
+    }
+
+    public IQueryable<TEntity> Get() => _context.Set<TEntity>();
+
+    public IQueryable<TEntity> Get(Expression<Func<TEntity, bool>> condition) => _context.Set<TEntity>().Where(condition);
+
+    public void Create(TEntity entity) => _context.Set<TEntity>().Add(entity);
+
+    public void Update(TEntity entity) => _context.Set<TEntity>().Update(entity);
+
+    public void Delete(TEntity entity) => _context.Set<TEntity>().Remove(entity);
+}
