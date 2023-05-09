55d8b62ae8e07378f4e3a62ce488ea37d1cd7bf0
Ivan Rozanov <navi.zor9@gmail.com>
2023-05-10 02:57:48 +0300

    add repository manager interface and implementation

---
+++ TransportTimetable.DAL/Interfaces/IRepositoryManager.cs
@@ -0,0 +1,12 @@
+﻿namespace TransportTimetable.DAL.Interfaces;
+
+public interface IRepositoryManager
+{
+    IRouteRepository Route { get; }
+    IStopRepository Stop { get; }
+    ITransportTypeRepository TransportType { get; }
+    ITimeTableRepository TimeTable { get; }
+
+    void Save();
+    Task SaveAsync();
+}
---
+++ TransportTimetable.DAL/Repositories/RepositoryManager.cs
@@ -0,0 +1,26 @@
+﻿using TransportTimetable.DAL.Interfaces;
+
+namespace TransportTimetable.DAL.Repositories;
+
+public class RepositoryManager : IRepositoryManager
+{
+    private readonly RepositoryContext _context;
+
+    private IRouteRepository? _routeRepository;
+    private IStopRepository? _stopRepository;
+    private ITransportTypeRepository? _transportTypeRepository;
+    private ITimeTableRepository? _timeTableRepository;
+
+    public RepositoryManager(RepositoryContext context)
+    {
+        _context = context;
+    }
+
+    public IRouteRepository Route => _routeRepository ??= new RouteRepository(_context);
+    public IStopRepository Stop => _stopRepository ??= new StopRepository(_context);
+    public ITransportTypeRepository TransportType => _transportTypeRepository ??= new TransportTypeRepository(_context);
+    public ITimeTableRepository TimeTable => _timeTableRepository ??= new TimeTableRepository(_context);
+
+    public void Save() => _context.SaveChanges();
+    public Task SaveAsync() => _context.SaveChangesAsync();
+}
