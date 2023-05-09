b9e6947ccaa0f2ab834615440e5fe0bb4a38ccd4
Ivan Rozanov <navi.zor9@gmail.com>
2023-05-09 19:31:29 +0300

    add repository context

---
+++ TransportTimetable.DAL/RepositoryContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TransportTimetable.DAL.Entities;
+
+namespace TransportTimetable.DAL;
+
+public class RepositoryContext : DbContext
+{
+    public DbSet<Route> Routes { get; set; }
+    public DbSet<Stop> Stops { get; set; }
+    public DbSet<TransportType> TransportTypes { get; set; }
+    public DbSet<TimeTable> TimeTables { get; set; }
+
+    public RepositoryContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+}
