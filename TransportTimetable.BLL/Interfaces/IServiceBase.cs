b712ac8d86bfbdcf2c31547cbddb3acc2f85f384
Ivan Rozanov <navi.zor9@gmail.com>
2023-05-11 23:53:00 +0300

    add service base

---
+++ TransportTimetable.BLL/Interfaces/IServiceBase.cs
@@ -0,0 +1,11 @@
+﻿namespace TransportTimetable.BLL.Interfaces;
+
+public interface IServiceBase<TDto>
+{
+    Task<IEnumerable<TDto>> GetAll();
+    Task<TDto?> GetById(Guid id);
+    Task Create(TDto dto);
+    Task Update(Guid id, TDto dto);
+    Task Delete(Guid id);
+    Task<bool> IsExist(Guid id);
+}
---
+++ TransportTimetable.BLL/Services/ServiceBase.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using TransportTimetable.BLL.Interfaces;
+using TransportTimetable.DAL.Interfaces;
+
+namespace TransportTimetable.BLL.Services;
+
+public abstract class ServiceBase<TDto> : IServiceBase<TDto>
+{
+    protected readonly IMapper Mapper;
+    protected readonly IRepositoryManager Repository;
+
+    protected ServiceBase(IMapper mapper, IRepositoryManager repository)
+    {
+        Mapper = mapper;
+        Repository = repository;
+    }
+
+    public abstract Task<IEnumerable<TDto>> GetAll();
+
+    public abstract Task<TDto?> GetById(Guid id);
+
+    public abstract Task Create(TDto dto);
+
+    public abstract Task Update(Guid id, TDto dto);
+
+    public abstract Task Delete(Guid id);
+    
+    public abstract Task<bool> IsExist(Guid id);
+}
