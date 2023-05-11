bc75983e75b6327f516069ed47c5f5ebd8d25509
Ivan Rozanov <navi.zor9@gmail.com>
2023-05-11 23:51:59 +0300

    add transport type dto

---
+++ TransportTimetable.BLL/DataTransferObjects/TransportTypeDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TransportTimetable.BLL.DataTransferObjects;
+
+public class TransportTypeDto
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = null!;
+}
---
+++ TransportTimetable/Mapper/DtoViewModelProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using TransportTimetable.BLL.DataTransferObjects;
+using TransportTimetable.ViewModels.TransportType;
+
+namespace TransportTimetable.Mapper;
+
+public class DtoViewModelProfile : Profile
+{
+    public DtoViewModelProfile()
+    {
+        CreateMap<TransportTypeDto, TransportTypeViewModel>().ReverseMap();
+    }
+}
