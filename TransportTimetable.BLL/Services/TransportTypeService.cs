5d38372dbb356035a2a32c5142c08caedeff3aaf
Ivan Rozanov <navi.zor9@gmail.com>
2023-05-16 05:43:50 +0300

    add transport type service

---
+++ TransportTimetable.BLL/Services/TransportTypeService.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TransportTimetable.BLL.DataTransferObjects;
+using TransportTimetable.BLL.Interfaces;
+using TransportTimetable.DAL.Entities;
+using TransportTimetable.DAL.Interfaces;
+
+namespace TransportTimetable.BLL.Services;
+
+public class TransportTypeService : ServiceBase<TransportTypeDto, TransportType>, ITransportTypeService
+{
+    public TransportTypeService(IMapper mapper, IRepositoryManager repositoryManager) : base(mapper, repositoryManager)
+    {
+    }
+}
